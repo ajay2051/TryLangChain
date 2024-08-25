3af17603beef70481ad7867de57c24cf9675e094
Ajay Thakur <ajaythk.94@gmail.com>
2024-08-25 13:32:51 +0545

    rate_limiting

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from fastapi import FastAPI
 from langserve import add_routes
 
 import langchain_chatbot
+from redis import Redis
 
 app = FastAPI()
-
+redis_instance = Redis(host='localhost', port=6379, password='<PASSWORD>', ssl=True)
 
 # /chain/playground
 # /chain/invoke
@@ -13,7 +14,19 @@ app = FastAPI()
 # /chain/batch
 @app.get("/")
 def home_page():
-    return {"Hello": "World"}
+    visits_key = "visits"
+    max_requests = 5
+    current_value = redis_instance.get(visits_key)
+    if current_value is None:
+        redis_instance.set(visits_key, 0, 10)
+    redis_instance.incr(visits_key)
+    final_value = redis_instance.get(visits_key)
+    do_rate_limiting = False
+    try:
+        do_rate_limiting = int(final_value) > max_requests
+    except ValueError:
+        pass
+    return {"Hello": "World", "visits": final_value, "limited": do_rate_limiting}
 
 
 chain = langchain_chatbot.get_chain()
